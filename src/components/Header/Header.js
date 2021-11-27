64bf0b85467124337744eed4f41252469cedc80d
Daniel C <azenis.ofc@gmail.com>
2021-11-27 21:34:30 +0100

    Exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
+
+const MenuButtons = ({ className }) => (
+  <ActionGroup className={className}>
+    <button>
+      <Search size={24} />
+    </button>
+    <button>
+      <Menu size={24} />
+    </button>
+  </ActionGroup>
+);
 
 const Header = () => {
   return (
     <header>
       <SuperHeader>
         <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
+          <MenuButtons />
           <ActionGroup>
             <button>
               <User size={24} />
@@ -29,7 +33,12 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <MenuButtonsDesktop />
         <Logo />
+        <SubscriberDiv>
+          <Button>Subscribe</Button>
+          <SubscriberLink href="#a">Already a subscriber?</SubscriberLink>
+        </SubscriberDiv>
       </MainHeader>
     </header>
   );
@@ -39,6 +48,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +78,54 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    justify-content: revert;
+    align-items: center;
+
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
+`;
+
+const MenuButtonsDesktop = styled(MenuButtons)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscriberDiv = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+    position: relative;
+    justify-self: end;
+  }
+`;
+
+const SubscriberLink = styled.a`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  margin-top: 8px;
+
+  color: var(--color-gray-900);
+
+  font-family: var(--font-family-serif);
+  font-size: ${14 / 16}rem;
+
+  text-decoration: underline;
+  font-style: italic;
 `;
 
 export default Header;
